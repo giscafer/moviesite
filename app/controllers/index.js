17f2730b152e8f80df8725d6b29d1cdb872d5c5b
giscafer <giscafer@outlook.com>
2015-08-29 11:45:02 +0800

    添加：全局关键字搜索功能

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,6 @@ var Category = require('../models/category')
 var Config=require('../../config');
     //index page
 exports.index = function(req, res) {
-    // console.log('---------------user in session-----------------');
-    // console.log(req.session.user);
     Category
         .find({})
         .populate({ //每个分类下取出5条 
@@ -26,30 +24,49 @@ exports.index = function(req, res) {
     //search page
 exports.search = function(req, res) {
     var catId=req.query.cat;
-    var page=Number(req.query.p);
+    var q=req.query.q;
+    var page=Number(req.query.p) || 0;
     var count=Config.pagecount || 2;
     var index=page * count;
-
-    Category
-        .find({_id:catId})
-        .populate({
-            path: 'movies',
-            select: 'title poster',
-        })
-        .exec(function(err, categories) {
-            if (err) {
-                console.log(err);
-            }
-            var category=categories[0] || {};
-            var movies=category.movies || [];
-            var results=movies.slice(index, index+count);
-            res.render('results', {
-                title: '结果列表页面',
-                keyword:category.name,
-                query:'cat='+catId,
-                currentPage:(page+1),
-                totalPage:Math.ceil(movies.length/count),
-                movies: results
+    if(catId){//分类id查询
+        Category
+            .find({_id:catId})
+            .populate({
+                path: 'movies',
+                select: 'title poster',
             })
-        });
+            .exec(function(err, categories) {
+                if (err) {
+                    console.log(err);
+                }
+                var category=categories[0] || {};
+                var movies=category.movies || [];
+                var results=movies.slice(index, index+count);
+                res.render('results', {
+                    title: '结果列表页面',
+                    keyword:category.name,
+                    query:'cat='+catId,
+                    currentPage:(page+1),
+                    totalPage:Math.ceil(movies.length/count),
+                    movies: results
+                });
+            });
+    }else{//关键字模糊查询
+        Movie.find({title:new RegExp(q+'.*','i')})
+            .exec(function(err,movies){
+                if(err) console.log(err);
+
+                 var results=movies.slice(index, index+count);
+
+                res.render('results', {
+                    title: '结果列表页面',
+                    keyword:q,
+                    query:'q='+q,
+                    currentPage:(page+1),
+                    totalPage:Math.ceil(movies.length/count),
+                    movies: results
+                });
+            })
+    }
+    
 }
